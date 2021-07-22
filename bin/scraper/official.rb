511de957e582146261d119e53dff57f455b92353
Tony Bowden <tony@tmtm.com>
2021-07-22 14:38:04 +0300

    Remove 'Mrs. ' prefix

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -9,7 +9,7 @@ class MemberList
   # details for an individual member
   class Member < Scraped::HTML
     field :name do
-      noko.xpath('./preceding-sibling::tr[@bgcolor="#FFCC99"]').last.text.tidy.gsub('Hon. ', '')
+      noko.xpath('./preceding-sibling::tr[@bgcolor="#FFCC99"]').last.text.tidy.gsub('Hon. ', '').gsub('(Mrs.) ', '')
     end
 
     field :position do
