20061cc423f3f836259adc4f504c6187458568fa
Tony Bowden <tony@tmtm.com>
2021-07-29 17:28:59 +0300

    Update to every-politician-scraper 0.0.21

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -2,7 +2,6 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/scraper_data'
-require 'open-uri/cached'
 require 'pry'
 
 class MemberList
@@ -31,5 +30,5 @@ class MemberList
   end
 end
 
-url = 'http://www.cabinetoffice.gov.lk/cab/index.php?option=com_content&view=article&id=25&Itemid=23&lang=en'
-puts EveryPoliticianScraper::ScraperData.new(url).csv
+file = Pathname.new 'html/official.html'
+puts EveryPoliticianScraper::FileData.new(file).csv
