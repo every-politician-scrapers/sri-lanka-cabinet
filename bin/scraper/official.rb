fc00b24662a87bf3d0f9e397506ea98ca6e34694
Tony Bowden <tony@tmtm.com>
2021-08-18 12:44:58 +0300

    Bump version to 0.1.1

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -5,28 +5,20 @@ require 'every_politician_scraper/scraper_data'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
-    field :name do
-      noko.xpath('./preceding-sibling::tr[@bgcolor="#FFCC99"]').last.text.tidy
-          .delete_prefix('His Excellency ')
-          .delete_prefix('Hon. ')
-          .gsub('(Mrs.) ', '')
+  class Member
+    def name
+      Name.new(
+        full: noko.xpath('./preceding-sibling::tr[@bgcolor="#FFCC99"]').last.text.tidy.gsub(/[()]/, ''),
+        prefixes: %w[His Excellency Hon Mrs Prof Dr]
+      ).short
     end
 
-    field :position do
+    def position
       noko.css('td').last.text.tidy
     end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.map { |member| fragment(member => Member).to_h }
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('#cab_normal_text_e').xpath('.//tr[@bgcolor="#FDE3CA"]')
     end
